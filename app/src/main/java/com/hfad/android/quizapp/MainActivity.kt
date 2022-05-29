9d1484874d873203da8318e4971a364a7fb32fc4
peto40 <petrosmalkhasyan13@gmail.com>
2022-05-29 10:04:13 +0400

    commit

--- app/src/main/java/com/hfad/android/quizapp/MainActivity.kt
+++ app/src/main/java/com/hfad/android/quizapp/MainActivity.kt
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity() {
             if (binding.nameEdt.text.toString().isEmpty()){
                 Toast.makeText(this,"Please enter your name",Toast.LENGTH_SHORT).show()
             } else {
+
+
+
                 val intent = Intent(this, QuizQuestionsActivity::class.java)
                 intent.putExtra(Constants.USER_NAME, binding.nameEdt.text.toString())
                 startActivity(intent)
