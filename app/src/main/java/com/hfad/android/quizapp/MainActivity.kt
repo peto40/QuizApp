0f9821fc671a7a21ae41bed1f48fdc9e572c96ef
peto40 <petrosmalkhasyan13@gmail.com>
2022-05-29 13:14:18 +0400

    add Log

--- app/src/main/java/com/hfad/android/quizapp/MainActivity.kt
+++ app/src/main/java/com/hfad/android/quizapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hfad.android.quizapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.hfad.android.quizapp.databinding.ActivityMainBinding
 
@@ -16,12 +17,12 @@ class MainActivity : AppCompatActivity() {
             if (binding.nameEdt.text.toString().isEmpty()){
                 Toast.makeText(this,"Please enter your name",Toast.LENGTH_SHORT).show()
             } else {
-
-
-
                 val intent = Intent(this, QuizQuestionsActivity::class.java)
                 intent.putExtra(Constants.USER_NAME, binding.nameEdt.text.toString())
                 startActivity(intent)
+
+
+                Log.d("hi","hello world!")
             }
 
         }
